721136b79b45976dfd3af7be39fba618525db387
sahil panchasara <sahilpanchasara822@gmail.com>
2022-12-19 13:42:10 +0530

    Initial Commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0xdB61625fCaF774993885D1Ce6Af1e2e68722D045";
+export const contractABI = abi.abi;
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,11 @@
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'alchemy url',
+      accounts: ['metamask url'],
+    },
+  },
+};
